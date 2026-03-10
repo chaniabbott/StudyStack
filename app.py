87235a10ca890911a700a0b9e711c8d7394ef9f4
ChanaAbbott <cabbott@student.touro.edu>
2026-03-10 19:41:00 +0200

    create .env.example

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+
+app.config['SECRET_KEY'] = os.environ.get('secret-key')
 
 @app.route('/')
 def home():
