94d8c2135a538eae0bf32235f14e4e22d229557e
ChanaAbbott <cabbott@student.touro.edu>
2026-04-05 18:25:42 -0400

    feat: created login and register

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
+from urllib.parse import urlsplit
 
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager, UserMixin, login_required, current_user,login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
-from wtforms import StringField, TextAreaField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, length, Length
-from flask_login import UserMixin
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, length, Length, EqualTo, ValidationError
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -15,6 +16,8 @@ app.config['SECRET_KEY'] = os.environ.get('CSRF_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI','sqlite:///studystack.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
 class Deck(db.Model):
@@ -26,7 +29,8 @@ class Deck(db.Model):
 
     cards = db.relationship('Card', backref='deck',lazy='select',
                             cascade='all, delete-orphan')
-    owner = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f'<Deck {self.title}>'
@@ -38,8 +42,7 @@ class User(UserMixin, db.Model):
     email=db.Column(db.String(120), unique=True, nullable=False)
     password=db.Column(db.String(255), nullable=False)
     created_at=db.Column(db.DateTime, server_default=db.func.now())
-    decks=db.relationship('Deck', backref='deck', lazy='select',
-                          cascade='all, delete-orphan')
+    decks=db.relationship('Deck', backref='owner', lazy='dynamic')
 
 
     def set_password(self, password):
@@ -75,6 +78,30 @@ class CardForm(FlaskForm):
     answer = TextAreaField('Answer', validators=[DataRequired()])
     submit = SubmitField('Add Card')
 
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Length(max=120)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('',validators=[DataRequired(), Length(min=8,max=50)])
+    submit = SubmitField('Login')
+
 with app.app_context():
     db.create_all()
 
@@ -82,19 +109,23 @@ with app.app_context():
 def home():
     return render_template('index.html')
 
+
 @app.route('/decks')
+@login_required
 def deck_list():
-    decks = Deck.query.order_by(Deck.created_at.desc()).all()
-    return render_template('index.html', decks=decks)
+    decks = Deck.query.filter_by(user_id=current_user.id).order_by(Deck.created_at.desc()).all()
+    return render_template('decks/index.html', decks=decks)
 
 @app.route('/decks/create', methods=['GET','POST'])
+@login_required
 def deck_create():
     form = DeckForm()
     if form.validate_on_submit():
         deck = Deck(
             title=form.title.data,
             description=form.description.data,
-            is_public=form.is_public.data
+            is_public=form.is_public.data,
+            user_id=current_user.id
         )
         db.session.add(deck)
         db.session.commit()
@@ -123,6 +154,45 @@ def card_create(deck_id):
         flash('Card added!', 'success')
         return redirect(url_for('deck_detail', deck_id=deck.id))
     return render_template('decks/new_card.html', form=form, deck=deck)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('deck_list'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('auth/register.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('deck_list'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password', 'danger')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('deck_list')
+        return redirect(next_page)
+    return render_template('auth/login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
 
 
 if __name__ == '__main__':
