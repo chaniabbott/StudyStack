f53cc755bced7c68d76705056d9f0117355c7239
ChanaAbbott <cabbott@student.touro.edu>
2026-04-24 01:58:23 +0300

    added sglalchemy line

--- config.py
+++ config.py
@@ -5,5 +5,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI','sqlite:///studystack.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = (
+            os.environ.get('DATABASE_URL')
+            or 'sqlite:///' + os.path.join(basedir, 'studystack.sqlite')
+    )
