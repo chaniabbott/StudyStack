6f9d9b8caca0f65112a3a06f8ca5020ea47195c7
ChanaAbbott <cabbott@student.touro.edu>
2026-04-23 13:05:50 +0300

    Fix SECRET_KEY config and update .env

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('CSRF_SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI','sqlite:///studystack.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
